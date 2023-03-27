3010e97892920168c159948ca045edfada1ddec9
GaryCraft <garycraft@our-space.xyz>
2023-03-26 22:39:05 -0600

    Remove unnecesary docker images

--- kratos-custom/get-dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt-get update -y && apt upgrade -y
-apt-get install wget curl git tar unzip jq -y
-apt-get install postgresql-client -y
-# Clear apt cache
-apt-get clean
--- kratos-custom/setup-nodejs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Node.js 16
-curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
-apt-get update -y
-apt-get install -y nodejs
-
-# Clear apt cache
-apt-get clean
